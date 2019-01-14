fc14dcb4d1477231199d1b960409c03799b7bae6
Kohei Tabata <nerd0geek1@gmail.com>
2019-01-14 13:45:50 +0900

    add logic to fetch iOS app metadata using appleID which user inputted

--- src/load-screenshot.js
+++ src/load-screenshot.js
@@ -1,8 +1,32 @@
 import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
-export default function() {
+export default function(context) {
   // default is 'Sketch Mirror' appleID
   const appleID = sketch.UI.getStringFromUser('Input iOS app ID which you want to load screenshot', 
                                               '677296955');
+  if (appleID == 'null') {
+    return;
+  }
+
+  const url = `https://itunes.apple.com/lookup?id=${appleID}&entity=software`;
+  fetch(url,{ method: 'GET' }).then(response => response.json())
+                              .then(json => {
+                                const appDetailJson = json['results'][0];
+                              })
+                              .catch(error => {
+                                sketch.UI.alert('Error😵', 'Failed to load iOS app screenshots.\nPlease confirm appleID what you inputted, network and so on.');
+                              });
+}
+
+function titleFromJSON(json) {
+  return json['trackName'];
+}
+
+function screenshotsURLFromJSON(json) {
+  return json['screenshotUrls'];
+}
+
+function screenshotsURLForiPadFromJSON(json) {
+  return json['ipadScreenshotUrls'];
 }
