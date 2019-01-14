0721d1e6793821a035169b0406a98edef87981e5
Kohei Tabata <nerd0geek1@gmail.com>
2019-01-14 13:13:13 +0900

    add logic to make user input appleID

--- src/load-screenshot.js
+++ src/load-screenshot.js
@@ -2,5 +2,7 @@ import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
 export default function() {
-  sketch.UI.message("It's alive 🙌")
+  // default is 'Sketch Mirror' appleID
+  const appleID = sketch.UI.getStringFromUser('Input iOS app ID which you want to load screenshot', 
+                                              '677296955');
 }
